f6fa759ccc93b5ff00f36e1e6eefd5495e026b43
Latif Altay <latifaltay00@gmail.com>
2024-07-17 20:20:36 +0300

    idk

--- ConsoleUI/obj/Debug/net8.0/ConsoleUI.AssemblyInfo.cs
+++ ConsoleUI/obj/Debug/net8.0/ConsoleUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10870237ef0e5d0579cf5d5e163c12c5ec85643f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
