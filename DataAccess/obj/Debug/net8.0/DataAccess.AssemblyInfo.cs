5aa2380064f1a1c67c667e51553c26e447e04e0e
Latif Altay <latifaltay00@gmail.com>
2024-07-22 02:25:17 +0300

    Core and Business Layers are updated (Messages, return types(Data result...))

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6fa759ccc93b5ff00f36e1e6eefd5495e026b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e00d9d13d06788a6185636352b477cf8c165e21")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
